80c3c3696b133d9bfe649893ad9547e20d697463
IMatheusmaia <i.matheuslmaia@gmail.com>
2024-08-12 11:38:12 -0300

    requisito 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,24 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
-
+from src.models.ingredient import Ingredient, Restriction
+import enum # noqa: F401, E261, E501
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient_1 = Ingredient("Cebola")
+    ingredient_2 = Ingredient("Cebola")
+
+    assert ingredient_1.name == "Cebola"
+    assert ingredient_1.restrictions == set()
+    assert repr(ingredient_1) == "Ingredient('Cebola')"
+    assert ingredient_1 == ingredient_2
+    assert hash(ingredient_1) == hash("Cebola")
+
+    ingredient_3 = Ingredient("bacon")
+    ingredient_4 = Ingredient("camarão")
+    
+    assert ingredient_3.name == "bacon"
+    assert ingredient_4.name == "camarão"
+
+    assert repr(ingredient_3) == "Ingredient('bacon')"
+    assert repr(ingredient_4) == "Ingredient('camarão')"
+    assert ingredient_3 != ingredient_4
+    assert hash(ingredient_3) == hash("bacon")
